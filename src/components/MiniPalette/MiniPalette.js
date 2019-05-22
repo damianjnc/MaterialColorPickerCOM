d3f3a164a1408a451e62a412e8ed8b6ba0725860
damianjnc <damianjnc@gmail.com>
2019-05-22 05:51:32 +0100

    finish styling minipalette

--- src/components/MiniPalette/MiniPalette.js
+++ src/components/MiniPalette/MiniPalette.js
@@ -1,47 +1,67 @@
-import React from 'react';
-import {withStyles} from '@material-ui/styles';
+import React from "react";
+import { withStyles } from "@material-ui/styles";
 
 const styles = {
-    root:{
-        backgroundColor: 'white',
-        borderRadius: '5px',
-        border:'1px solid black',
-        padding: '0.5rem',
-        position:'relative',
-        overflow:'hidder',
-        "&:hover":{
-            cursor:'pointer'
-        }
-    },
-    emoji:{
-        marginLeft:'0.5rem',
-        fontSize: '1.5rem'
-    },
-    colors:{
-        backgroundColor: 'grey'
-    },
-    title:{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems:'center',
-        margin:'0',
-        color:'black',
-        paddingTop: '0.5rem',
-        fontSize:'1rem',
-        position:'relative'
+  root: {
+    backgroundColor: "white",
+    borderRadius: "5px",
+    border: "1px solid black",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidder",
+    "&:hover": {
+      cursor: "pointer"
     }
-
-}
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem"
+  },
+  colors: {
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden"
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative"
+  },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px"
+  }
+};
 
 function MiniPalette(props) {
-    const {classes} = props;
-    return(
-        <div className={classes.root}>
-            <div className={classes.colors}/>
-            <h5 className={classes.title}>{props.paletteName}
-            <span className={classes.emoji}>{props.emoji}</span></h5>
-        </div>
-    );
+  const { classes, colors } = props;
+  const miniColorBoxes = colors.map(color => (
+    <div
+      key={color.name}
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+    />
+  ));
+  return (
+    <div className={classes.root}>
+      <div className={classes.colors}>{miniColorBoxes}</div>
+      <h5 className={classes.title}>
+        {props.paletteName}
+        <span className={classes.emoji}>{props.emoji}</span>
+      </h5>
+    </div>
+  );
 }
 
 export default withStyles(styles)(MiniPalette);
