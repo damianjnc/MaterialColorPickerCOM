5e52b36d80c59422acd1572acba81cf495e9b818
damianjnc <damianjnc@gmail.com>
2019-05-22 06:43:30 +0100

    add link to palette

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-import React, {Component} from "react";
-import {Route, Switch} from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./components/Palette/Palette";
 import seedColors from "./components/seedColors";
-import {generatePalette} from "./components/colorHelpers";
+import { generatePalette } from "./components/colorHelpers";
 import PaletteList from "./components/PaletteList/PaletteList";
 
 class App extends Component {
-    findPalette = id => {
-        return seedColors.find(function (element) {
-            return element.id === id;
-        });
-    };
+  findPalette = id => {
+    return seedColors.find(function(element) {
+      return element.id === id;
+    });
+  };
 
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/" render={() => <PaletteList palettes={seedColors}/>}/>
-                <Route
-                    exact
-                    path="/palette/:id"
-                    render={(routeProps) => <Palette
-                        palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}
-                />
-            </Switch>
-            /* <div>
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      /* <div>
               <Palette palette={generatePalette(seedColors[4])} />
             </div> */
-        );
-    }
+    );
+  }
 }
 
 export default App;
